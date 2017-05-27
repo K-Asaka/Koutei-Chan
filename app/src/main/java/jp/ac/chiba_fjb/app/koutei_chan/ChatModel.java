124e6c47b5ff2eaf167ca3cfba83467a3bf84b3e
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2017-05-27 14:46:34 +0900

    PostDataとそれを継承するPostText、PostImageクラスを追加。中身はまだない。

--- app/src/main/java/jp/ac/chiba_fjb/app/koutei_chan/ChatModel.java
+++ app/src/main/java/jp/ac/chiba_fjb/app/koutei_chan/ChatModel.java
@@ -1,9 +1,39 @@
 package jp.ac.chiba_fjb.app.koutei_chan;
 
+import android.media.Image;
+
+
 /**
  * Created by asaka on 2017/05/27.
  */
 
 public class ChatModel {
 
+    public ChatModel() {
+
+    }
+
+    public void post(PostData postData) {
+        // TODO: 投稿されたデータに対する処理を追加する
+
+        // DBへ正常に追加されたかどうかのチェック
+    }
+
+    public PostData reply() {
+
+    }
+
 }
+
+class PostData {
+}
+
+class PostText extends PostData {
+    public PostText(String message) {
+        super();
+    }
+}
+
+class PostImage extends PostData {
+}
+
