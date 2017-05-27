837464c844430f552144969048f6a2876010b7a6
kawashi <aisatu_nomahou@yahoo.co.jp>
2017-05-27 17:03:43 +0900

    Activityをパッケージで分けた

--- app/src/main/java/jp/ac/chiba_fjb/app/koutei_chan/Activity/MainActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/app/koutei_chan/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package jp.ac.chiba_fjb.app.koutei_chan;
+package jp.ac.chiba_fjb.app.koutei_chan.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import jp.ac.chiba_fjb.app.koutei_chan.R;
 
 public class MainActivity extends AppCompatActivity {
 
