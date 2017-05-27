f84715d807d271aab8f23556c05cf5bad7d6e8db
kawashi <aisatu_nomahou@yahoo.co.jp>
2017-05-27 18:22:25 +0900

    Realmの初期化処理を追加

---
+++ app/src/main/java/jp/ac/chiba_fjb/app/koutei_chan/Activity/MyApplication.java
@@ -0,0 +1,17 @@
+package jp.ac.chiba_fjb.app.koutei_chan.Activity;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(this);
+        RealmConfiguration config = new RealmConfiguration.Builder().build();
+        Realm.setDefaultConfiguration(config);
+    }
+}
