94cc94913877a0ed3954d1e52c658bdfc29b0f24
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2017-05-27 15:35:12 +0900

    UserMessageクラスの命名を変えてChatModelクラスから分離。

--- app/src/main/java/jp/ac/chiba_fjb/app/koutei_chan/ChatModel.java
+++ app/src/main/java/jp/ac/chiba_fjb/app/koutei_chan/ChatModel.java
@@ -1,43 +1,23 @@
 package jp.ac.chiba_fjb.app.koutei_chan;
 
-import android.media.Image;
-
-
-/**
- * Created by asaka on 2017/05/27.
- */
-
 public class ChatModel {
+
+    private Bot botInstance;
 
     public ChatModel() {
 
     }
 
-    public void post(UserMessage userMessage) {
+    public void post(Message userMessage) {
         // TODO: 投稿されたデータに対する処理を追加する
 
         // DBへ正常に追加されたかどうかのチェック
     }
 
-    public UserMessage reply() {
+    public Message reply() {
 
     }
 
-}
-
-class UserMessage {
-}
-
-class UserText extends UserMessage {
-    public UserText(String message) {
-        super();
-    }
-}
-
-class UserImage extends UserMessage {
-    public UserImage() {
-        super();
-    }
 }
 
 
---
+++ app/src/main/java/jp/ac/chiba_fjb/app/koutei_chan/Message.java
@@ -0,0 +1,17 @@
+package jp.ac.chiba_fjb.app.koutei_chan;
+
+
+public class Message {
+}
+
+class TextMessage extends Message {
+    public UserText(String message) {
+        super();
+    }
+}
+
+class ImageMessage extends Message {
+    public UserImage() {
+        super();
+    }
+}
