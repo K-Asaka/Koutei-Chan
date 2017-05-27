7255bf743f355922f34983e132cdb125770049eb
kawashi <aisatu_nomahou@yahoo.co.jp>
2017-05-27 18:15:22 +0900

    DB関連のクラスを作成

---
+++ app/src/main/java/jp/ac/chiba_fjb/app/koutei_chan/DB/Bot.java
@@ -0,0 +1,18 @@
+package jp.ac.chiba_fjb.app.koutei_chan.DB;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Bot extends RealmObject {
+    @PrimaryKey
+    private int id;
+    private String phrase;
+    private int mode;
+
+    public int    getId() { return this.id; }
+    public void   setId(int id) { this.id = id; }
+    public String getPhrase() { return this.phrase; }
+    public void   setPhrase(String phrase) { this.phrase = phrase; }
+    public int    getMode() { return this.mode; }
+    public void   setMode(int mode) { this.mode = mode; }
+}
---
+++ app/src/main/java/jp/ac/chiba_fjb/app/koutei_chan/DB/Mode.java
@@ -0,0 +1,8 @@
+package jp.ac.chiba_fjb.app.koutei_chan.DB;
+
+public class Mode {
+    private int mode;
+
+    public int  getMode() { return this.mode; }
+    public void setMode(int mode) { this.mode = mode; }
+}
