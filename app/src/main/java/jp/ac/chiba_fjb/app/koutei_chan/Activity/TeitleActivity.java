9cd47991c8b46648e3579dafb81d33f02daa04c4
javaSone <marirei398@yahoo.co.jp>
2017-05-28 11:19:31 +0900

    タイトル追加しました

--- app/src/main/java/jp/ac/chiba_fjb/app/koutei_chan/Activity/TeitleActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/app/koutei_chan/Activity/TeitleActivity.java
@@ -1,7 +1,11 @@
-package jp.ac.chiba_fjb.app.koutei_chan;
+package jp.ac.chiba_fjb.app.koutei_chan.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import jp.ac.chiba_fjb.app.koutei_chan.R;
 
 public class TeitleActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class TeitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teitle);
+    }
+    public  void onClick(View view){
+        Intent intent =new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
